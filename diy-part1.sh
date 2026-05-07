f5cabf57969a64a26aa02593b7963ba8a05af62a
Andrew <40738794+ByteJumper@users.noreply.github.com>
2026-05-07 20:35:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,8 @@ echo "==> DIY Part 1 Start..."
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '/helloworld/d' feeds.conf.default
 
+echo 'src-git ddns-go https://github.com/sirpdboy/luci-app-ddns-go' >> feeds.conf.default
+
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 # echo 'src-git helloworld https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
